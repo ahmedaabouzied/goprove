488cac2dfd2ddee51474332143f5808ac8ebbd49
Ahmed Abouzied <email@aabouzied.com>
2026-03-02 23:45:18 +0100

    Implement a CLI command that analyzes functions
    Signed-off-by: Ahmed Abouzied <email@aabouzied.com>

--- cmd/goprove/prover.go
+++ cmd/goprove/prover.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 
+	"github.com/ahmedaabouzied/goprove/pkg/analysis"
 	"github.com/ahmedaabouzied/goprove/pkg/loader"
 	"golang.org/x/tools/go/ssa"
 )
@@ -21,66 +21,44 @@ func provePackage(path string) error {
 	}
 
 	for _, pkg := range pkgs {
-		if err := printPkg(pkg); err != nil {
+		if err := analyzePkg(pkg); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func printPkg(pkg *ssa.Package) error {
+func analyzePkg(pkg *ssa.Package) error {
 	for _, member := range pkg.Members {
 		fn, ok := member.(*ssa.Function)
 		if !ok {
 			continue
 		}
-		if err := printFunction(fn); err != nil {
+		if err := analyzeFunction(fn); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func printFunction(fn *ssa.Function) error {
-	// Signature
+func analyzeFunction(fn *ssa.Function) error {
 	w := bufio.NewWriter(os.Stdout)
-	fmt.Fprintf(w, "func %s:\n", fn.Name())
-
-	for i, block := range fn.Blocks {
-		// Predecessor/Successor indices
-		predIdxs := make([]string, len(block.Preds))
-		for j, p := range block.Preds {
-			predIdxs[j] = fmt.Sprintf("%d", p.Index)
-		}
-		succIdxs := make([]string, len(block.Succs))
-		for j, s := range block.Succs {
-			succIdxs[j] = fmt.Sprintf("%d", s.Index)
-		}
-
-		comment := ""
-		if i == 0 {
-			comment = " (entry)"
-		}
-		fmt.Fprintf(w, "  Block %d:%s\n", block.Index, comment)
-
-		fmt.Fprintf(w, "    Succs: [%s]  Preds: [%s]\n",
-			strings.Join(succIdxs, ", "),
-			strings.Join(predIdxs, ", "))
-
-		for _, instr := range block.Instrs {
-			instrType := fmt.Sprintf("%T", instr)
-			// Strip the "*ssa." prefix
-			instrType = strings.TrimPrefix(instrType, "*ssa.")
-
-			// If the instruction produces a value, show the assignment
-			if val, ok := instr.(ssa.Value); ok {
-				fmt.Fprintf(w, "      %-30s (%s)\n",
-					fmt.Sprintf("%s = %s", val.Name(), instr),
-					instrType)
-			} else {
-				fmt.Fprintf(w, "      %-30s (%s)\n", instr, instrType)
-			}
-		}
+	analyzer := analysis.Analyzer{}
+	findings := analyzer.Analyze(fn)
+	for _, finding := range findings {
+		printFinding(w, finding)
 	}
 	return w.Flush()
+}
+
+func printFinding(w *bufio.Writer, finding analysis.Finding) {
+	switch finding.Severity {
+	case analysis.Bug:
+		fmt.Fprintf(w, "\033[31m %d %s \n", finding.Pos, finding.Message)
+	case analysis.Warning:
+		fmt.Fprintf(w, "\033[33m %d %s \n", finding.Pos, finding.Message)
+	case analysis.Safe:
+		fmt.Fprintf(w, "\033[32m %d %s \n", finding.Pos, finding.Message)
+	default:
+	}
 }
